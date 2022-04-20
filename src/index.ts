60c3600a2869fb58bd2308c7341119ea4f2d65e6
wonseok-han <oshan1112@gmail.com>
2022-04-20 11:02:48 +0900

    Object 사용 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
+interface PersonProps {
+  name: string;
+  age: number;
+  gender: string;
+}
 const name = "test",
   age = 29,
   gender = "male";
 
+const person: PersonProps = {
+  name,
+  age,
+  gender,
+};
+
 const sayHi = (name: string, age: number, gender?: string): void => {
+  console.log("this function is sayHi!!");
   console.log("name::", name);
   console.log("age::", age);
   console.log("gender::", gender);
 };
 
-sayHi(name, age);
+const giveMeObject = ({ name, age, gender }: PersonProps) => {
+  console.log("this function is giveMeObject!!");
+  console.log("name::", name);
+  console.log("age::", age);
+  console.log("gender::", gender);
+};
+
+// sayHi(name, age);
+giveMeObject(person);
 
 export {};
